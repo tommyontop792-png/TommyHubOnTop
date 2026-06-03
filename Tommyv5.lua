899f65eae4f3b5390991ed6b8bbfe457f026634b
tommyontop792-png <tommyontop792@gmail.com>
2026-06-03 14:03:19 -0600

    Update Tommyv5.lua

--- Tommyv5.lua
+++ Tommyv5.lua
@@ -1,1 +1,11 @@
-puto adbiel no pases la version
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+local razonDeBan = [[You have been banned from this experience.
+
+Reason: Violating Community Standards - Exploiting (Error Code: por tu puta madre)
+Ban Expiration: Permanent
+Moderator ID: 1928374]]
+
+task.wait(0.8)
+player:Kick(razonDeBan)
